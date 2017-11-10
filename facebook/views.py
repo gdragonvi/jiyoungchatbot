199868329b9adb1fbda9c2e3f355a809d85eb208
YoungJi <gdragonvi@naver.com>
2017-11-10 16:39:58 +0900

    fsf

--- facebook/views.py
+++ facebook/views.py
@@ -32,7 +32,7 @@ def web_hook(request):
                     elif text =='나이':
                         send_message = Message(text='10')
                     else:
-                        send_message = Message(text='send_message')
+                        send_message = Message(text=send_message)
 
                     req.send_message(RequestDataFormat(recipient=Recipient(recipient_id=receiver),
                                                            message=send_message))
