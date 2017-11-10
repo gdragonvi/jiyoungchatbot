683c39a8520c60f9618048fa8285c1fd268318df
YoungJi <gdragonvi@naver.com>
2017-11-10 16:18:41 +0900

    fsf

--- facebook/views.py
+++ facebook/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 import json
+from blueforge.apis.facebook import CreateFacebookApiClient, RequestDataFormat, Recipient, Message
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 verify_token = 'testest'
-access_token = 'insert_page_access_token'
+access_token = 'EAAcD9VAasYwBANt3KcuSfusZCaCPUQi6lhYXAvg8JuLZC7U6znuWKPkj9DHz3UBsYaKNARsIVB4l1WBEHaDp9uVHUfMA8j3xPYaLDjMwFE8qVqoSC8gRmpKTvrQc7ZCLo9j4L2zqJaeti5tpcyp2rmP7qHZAcNMp3SlZARZAHI7QZDZD'
+
+req = CreateFacebookApiClient(access_token=access_token)
 
 
 @csrf_exempt
@@ -18,5 +21,13 @@ def web_hook(request):
     elif request.method == 'POST':
         print(request.body)
         data = json.loads(request.body.decode('UTF-8'))
+
+        for entry in data['entry']:
+            for message in entry['messaging']:
+                receiver = message['sender']['id']
+                if 'message' in message:
+                    text = message['message']['text']
+                    req.send_message(RequestDataFormat(recipient=Recipient(recipient_id=receiver),
+                                                       message=Message(text=text)))
         return JsonResponse(data)
     return HttpResponse('Failed to request', status=404)
